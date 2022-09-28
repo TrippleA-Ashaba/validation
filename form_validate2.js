e44ae7fbd08272e3141549b158a1d717a5f47731
TrippleA-Ashaba <trippleaunit@hotmail.com>
2022-09-28 12:20:34 +0300

    form_validate2.js

---
+++ form_validate2.js
@@ -0,0 +1,14 @@
+const userName = document.registration.username;
+const emailEl = document.registration.email;
+const telephone = document.registration.phonenumber;
+const gender = document.registration.gender;
+const engLang = document.registration.english;
+const lugLang = document.registration.luganda;
+const form = document.registration.signup;
+
+const min = 3;
+const max = 25;
+
+// alphabet =/^[A-Za-z]+$/
+// numbers =/^[0-9]+$/
+// email =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
